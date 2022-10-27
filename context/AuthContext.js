2b08f7d3dcf7a0dcdc29ef0e89ae65c871852ca9
jishnukoottala <jishnu61@gmail.com>
2022-10-27 19:50:24 +0530

    resuable auth

---
+++ context/AuthContext.js
@@ -0,0 +1,65 @@
+import React, {useContext, useState, useEffect, useRef } from "react";
+import { auth, db } from "../firebase"
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword,
+     signOut, onAuthStateChanged, updateProfile} from 'firebase/auth';
+     import { doc, getDoc } from "firebase/firestore";
+
+
+     const AuthContext = React.createContext()
+
+
+     export function useAuth(){
+
+        return useContext(AuthContext);
+
+     }
+
+     
+
+
+     export function AuthProvider({children}){
+        const [currentUser, setCurrentUser]= useState(null);
+        const [loading, setLoading] = useState(true);
+        const userInfo = useRef()
+
+        function updateDisplayName(displayName){
+            return updateProfile(auth.currentUser, { displayName });
+        }
+
+        function signup(email,password){
+            createUserWithEmailAndPassword(auth,email, password);
+            return;
+        }
+
+        function login(email,password){
+            return signInWithEmailAndPassword(auth,email,password);
+
+        }
+
+        function logout(){
+            return signOut(auth)
+        }
+
+        useEffect(()=>{
+            const unsubscribe = onAuthStateChanged(auth, async user => {
+                setCurrentUser(user);
+                setLoading(false)
+            })
+            return unsubscribe
+        },[])
+
+        const value = {
+            currentUser,
+            login,
+            signup,
+            logout,
+            updateDisplayName,
+            userInfo
+        }
+
+        return (
+            <AuthContext.Provider value={value}>
+                {!loading && children}
+            </AuthContext.Provider>
+        )
+     }
