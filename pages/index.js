c35fdc014a36008aabc5b52278e276e01476e49b
jishnukoottala <jishnu61@gmail.com>
2022-10-27 19:51:36 +0530

    index page for home

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from 'next/head'
 import Image from 'next/image';
 import Login from '../components/Login';
+import { useAuth } from '../context/AuthContext';
+import UserDashBoard from '../components/UserDashBoard';
 
 export default function Home() {
+  const { currentUser } = useAuth();
+  console.log("currentUser",currentUser)
   return (
     <>
       <Head>
         <title>Project Lister</title>
       </Head>
      
-      <Login />
+      {!currentUser && <Login />}
+      {currentUser && <UserDashBoard /> }
     </>
   )
 }
