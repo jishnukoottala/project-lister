83904a63c4ab7613bc2a6330e7450edcc342176e
jishnukoottala <jishnu61@gmail.com>
2022-10-27 19:48:54 +0530

    create project top header to dsiplay filters and buttons

---
+++ components/Project/CreateProject.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../Button'
+
+export default function CreateProject() {
+  return (
+    <div className="border border-solid border-grey-400 p-3 text-right flex justify-end">
+      <div><Button className="" asLink href="/project"> Create Project</Button></div>
+    </div>
+  )
+}
