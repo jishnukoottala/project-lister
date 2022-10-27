785f7961d25d9a1077831413d87a461329d737bb
jishnukoottala <jishnu61@gmail.com>
2022-10-27 19:48:34 +0530

    user dashboard

---
+++ components/UserDashBoard.js
@@ -0,0 +1,17 @@
+import { useAuth } from "../context/AuthContext"
+import CreateProject from "./Project/CreateProject"
+import ProjectListing from "./Project/ProjectListing"
+
+
+export default function UserDashBoard() {
+
+    const { currentUser } = useAuth()
+  return (
+    <div>
+        <CreateProject />
+        <ProjectListing />
+        
+      
+    </div>
+  )
+}
