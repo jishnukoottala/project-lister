0bfd2cc590c17c396b892ad9791af66347bf37a6
jishnukoottala <jishnu61@gmail.com>
2022-10-28 18:06:09 +0530

    fetch Project

--- hooks/project/fetchProject.js
+++ hooks/project/fetchProject.js
@@ -1,7 +1,18 @@
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, doc,getDoc,  getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { db } from "../../firebase";
+
+const dateOptions = { weekday: 'short', month:'short', day: 'numeric', year: 'numeric'};
+const timeOptions = { hour: '2-digit', minute:'2-digit'}
+
+const dateTimeDisplay = (docDate) => {
+    if(docDate){
+        const date = docDate.toDate();
+        return `${date.toLocaleDateString('en-GB', dateOptions)} ${date.toLocaleTimeString('en-GB',timeOptions)}`
+    }
+    return ""
+}
 
 export const useFetchProjects = () => {
 
@@ -11,14 +22,21 @@ export const useFetchProjects = () => {
 
     const { currentUser } = useAuth();
 
+ 
+
     const fetchData = async () => {
         try{
 
             
             const querySnapshot =  await getDocs(collection(db, "projects"));
             const projectsLocal = []
+
+            
           
-            querySnapshot.forEach((doc)=> projectsLocal.push({...doc.data(), createdAt:doc.data()?.createdAt?.toDate()?.toDateString(), id: doc.id }))
+            querySnapshot.forEach((doc)=> projectsLocal.push({...doc.data(), 
+                createdAt:dateTimeDisplay(doc.data()?.createdAt),
+                updatedAt:dateTimeDisplay(doc.data()?.updatedAt),
+                id: doc.id }))
             setProjects(projectsLocal);
 
         }catch(err){
@@ -34,4 +52,39 @@ export const useFetchProjects = () => {
     },[])
 
     return { loading, error, projects }
+}
+
+
+export const useFetchProject = (id) => {
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [project, setProject] = useState(null);
+
+    const fetchData = async () => {
+        try{
+
+            
+            const querySnapshot =  await getDoc(doc(db, "projects", id));
+
+            if(querySnapshot.exists()){
+            setProject({...querySnapshot.data(), 
+                createdAt:dateTimeDisplay(querySnapshot.data()?.createdAt),
+                updatedAt:dateTimeDisplay(querySnapshot.data()?.updatedAt)
+            })
+            }
+
+        }catch(err){
+            setError('Failed to load projects')
+            console.log("projects loading --",err)
+        }finally{
+            setLoading(false);
+        }
+    }
+    useEffect(()=>
+    {
+        fetchData()
+    },[])
+
+    return { loading, error, project }
 }
