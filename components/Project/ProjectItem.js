3c39f9021577ed46f5c5452e196864b66cb9c76b
jishnukoottala <jishnu61@gmail.com>
2022-10-28 18:06:00 +0530

    edit support

--- components/Project/ProjectItem.js
+++ components/Project/ProjectItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 export default function ProjectItem({ id , workflow, projectName, status, userName, createdAt, updatedAt  }) {
 
+    const router = useRouter()
     const itemClassName = "text-base text-slate-800 flex justify-start p-2"
     const headingClassName = "font-semibold sm:hidden pr-2"
 
@@ -13,7 +15,7 @@ export default function ProjectItem({ id , workflow, projectName, status, userNa
         <div className={itemClassName}><h1 className={headingClassName}>Created By: </h1>{userName}</div>
         <div className={itemClassName}><h1 className={headingClassName}>Created At: </h1>{createdAt}</div>
         <div className={itemClassName}><h1 className={headingClassName}>Updated At: </h1>{updatedAt}</div>
-        <div className={`${itemClassName} border-b border-gray-500 sm:border-0 pb-3`}><i class="fa-solid fa-pen-to-square hover:cursor-pointer"></i></div>
+        <div className={`${itemClassName} border-b border-gray-500 sm:border-0 pb-3`} onClick={()=> router.push(`/project/${id}`)}><i className="fa-solid fa-pen-to-square hover:cursor-pointer"></i></div>
       
     </>
   )
