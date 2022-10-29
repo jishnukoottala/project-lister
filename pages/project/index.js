0f687b81f076d04ce6ee532397425e6bd3b3d448
jishnukoottala <jishnu61@gmail.com>
2022-10-29 12:21:40 +0530

    createLoading

--- pages/project/index.js
+++ pages/project/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectForm from '../../components/Project/ProjectForm'
 import { createProject  } from "../../hooks/project/createProject";
 import { useRouter } from 'next/router'
@@ -9,13 +9,17 @@ export default function Projects() {
 
   const router = useRouter()
   const { currentUser } = useAuth()
+  const [loading, setLoading] = useState(false)
 
   const onUpdateHandler = async (_, projectData)=> {
     try {
       console.log("projectData", { currentUser, projectData})
+      setLoading(true);
       const projectRef = await createProject(projectData,currentUser)
       console.log("projectId  ", projectRef.id)
+      setLoading(false);
       router.push("/")
+
     }catch(err){
 
     }
@@ -23,7 +27,7 @@ export default function Projects() {
 
   return (
     <div>
-      <ProjectForm onUpdateHandler={onUpdateHandler} />
+      <ProjectForm onUpdateHandler={onUpdateHandler} createLoading={loading}  />
     </div>
   )
 }
