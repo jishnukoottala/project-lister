d142f49cbe7c9b16e1c627031fcfa267f16e5be6
jishnukoottala <jishnu61@gmail.com>
2022-10-29 12:20:54 +0530

    pagination support to table

--- components/Project/ProjectListing.js
+++ components/Project/ProjectListing.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useFetchProjects } from "../../hooks/project/fetchProject";
+import Pagination from '../Pagination';
 import ProjectItem from './ProjectItem';
 
 export default function ProjectListing() {
 
-  const { loading, error, projects } = useFetchProjects()
+  const { loading, error, projects, page,  fetchNext, fetchPrevious } = useFetchProjects()
 
-  console.log("projects",projects)
   return (
     <div className="mt-5 container mx-auto px-4 flex flex-col justify-center">
       
@@ -21,8 +21,11 @@ export default function ProjectListing() {
         </div>
        
         <div className="grid grid-cols-1 sm:grid-cols-7 gap-x-1 gap-y-2">
-         {projects?.length &&  projects.map((project)=> (<ProjectItem key={project.id} {...project} />))} </div>
-    
+         {projects?.length > 0 && !loading &&  projects.map((project)=> (<ProjectItem key={project.id} {...project} />))} </div>
+         {projects?.length > 0 && !loading && (<Pagination active={true} 
+         showPrevious={page > 1}
+         fetchNext={fetchNext} fetchPrevious={fetchPrevious} showNext={!(projects?.length < 5)} />)}
+         
     </div>
   )
 }
