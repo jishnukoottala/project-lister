463e919c3b7cfdbac024348d6f705d8b59123635
jishnukoottala <jishnu61@gmail.com>
2022-12-25 09:17:07 +0530

    removed comment

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
+
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_APIKEY,
     authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
